d83cd3888ca7a4ef97b7cc1bd8c64aec10d0b430
TraxDinosaur <ImTraxDinosaur@gmail.com>
2024-09-05 22:59:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         scriptingDone: true,
         shootingDone: true,
         editingDone: true,
-        reviewDone: true
+        reviewDone: false
     };
 
     // Update UI with mock data
